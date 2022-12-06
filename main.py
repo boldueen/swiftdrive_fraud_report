1bc43c356a3d657d2a59e729cabd4d091bc12bce
Ivan Nikonov <nikon2283@gmail.com>
2022-12-06 19:18:52 +0300

    calling functions

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from settings import chmod_to_geckodriver
 
 from downloader import download_orders_file
 
+from reporter import create_report_from_orders
+
+from gshetts import set_df_to_gsheet
+
 
 
 if __name__ == "__main__":
@@ -11,7 +15,15 @@ if __name__ == "__main__":
     if chmod_to_geckodriver('./utils/geckodriver'):
         logger.info("chmod +x for geckodriver")
 
-    orders_filepath = download_orders_file('2022-01-01', '2022-01-07')
+    # orders_filepath = download_orders_file('2022-10-10', '2022-10-15')
+    # print(orders_filepath)
+    fraud_rides = create_report_from_orders('downloads/4010d385-41ba-4eb6-a73e-9639e9b3b2ee.xlsx')
+    # print(fraud_rides.info())
+
+    set_df_to_gsheet(fraud_rides)    
+
+
+
     # TODO create report
     # TODO import report to google sheets
     # TODO send gooogke_sheet link to emails
