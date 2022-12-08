9b716e57b9790e0b115d1917cf0199f64979f794
boldueen <nikon2283@gmail.com>
2022-12-08 21:25:26 +0300

    rename titles of df

--- reporter.py
+++ reporter.py
@@ -4,7 +4,16 @@ import pandas as pd
 def create_report_from_orders(orders_filepath: str):
     df = pd.read_excel(orders_filepath)
 
+    df.rename({
+        'Расстояние':'distance',
+        'Время выполнения':'drive_time',
+        'Стоимость':'ride_cost',
+        'Платное ожидание':'waiting_time',
+        'Телефон':'phone'
+        }, axis=1, inplace=True)
 
+    df = df.astype({'phone': str})
+    df.info()
     time_to_wait = 20
 
     day_down_spread_limit = 35
@@ -14,12 +23,22 @@ def create_report_from_orders(orders_filepath: str):
     night_up_spread_limit = 50
 
 
-    correct_rides_df = df[df['Статус'] == 'Заказ завершен успешно']
 
 
-    test = df[df['Стоимость'] > 2000]
+    # correct_rides_df = df[df['Статус'] == 'Заказ завершен успешно']
+    correct_rides_df = df
 
-    return test
+
+    correct_rides_df['avg_speed'] = correct_rides_df.distance / (correct_rides_df.drive_time*60)
+    correct_rides_df = correct_rides_df[correct_rides_df.avg_speed > 0]
+    correct_rides_df.info()
+    correct_rides_df.head(5)
+    
+    
+
+    # test = df[df['Стоимость'] > 2000]
+
+    return correct_rides_df
         
 
 
